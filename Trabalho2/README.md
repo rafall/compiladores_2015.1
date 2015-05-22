921b16bdbb68fe2d996c0226508fd154c7cd198a
Hiroki Minami <rafael.hiroki@gmail.com>
2015-05-22 19:05:34 -0300

    Primeiro commit do trabalho 2

---
+++ Trabalho2/README.md
@@ -0,0 +1,32 @@
+# Compiladores
+##### Repositório para os trabalhos da disciplina de Compiladores
+
+Guilherme Barranco Piva - nUSP: 7696462
+
+Guilherme Lustri de Oliveira - nUSP: 7656492
+
+Rafael Hiroki de Figueiroa Minami - nUSP: 7573187
+
+
+
+#### Instalar LEX FLEX no LINUX:
+
+`sudo apt-get install byacc flex`
+
+#### Compilar:
+
+`make`
+
+#### Testar:
+
+##### Rodar todos os testes (Apenas cria os arquivos, não mostra os resultados na saída padrão):
+
+`make test`
+
+##### Rodar testes separadamente (Cria o arquivo de saída e também mostra na saída padrão):
+
+`make test1`
+
+`make test2`
+
+`make test3`
---
+++ Trabalho2/hashmap.c
@@ -0,0 +1,222 @@
+/*
+* SCC0217 Linguagens de programação e compiladores
+* Professor Diego Raphael Amancio
+* Trabalho 1: Analisador Léxico
+*********************************************************
+* Alunos:
+* 
+* Guilherme Barranco Piva					nUSP: 7696462
+* Guilherme Lustri de Oliveira				nUSP: 7656492
+* Rafael Hiroki de Figueiroa Minami			nUSP: 7573187
+*********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hashmap.h"
+
+extern int yylex(); 
+extern int yylineno;
+extern char *yytext;
+
+int main(int argc, char *argv[])
+{
+	int ntoken, vtoken;
+
+	initTable();
+
+	// ntoken guarda o indice token retornado do analisador lexico
+	ntoken = yylex();
+
+	while(ntoken)
+	{
+		switch(ntoken)
+		{
+			case INTEIROGRANDEDEMAIS:
+				printf("Erro: Inteiro muito grande. Linha: %d\n", yylineno);
+				break;
+			case NUMERO_INT:
+				printf("%s - Numero inteiro\n", yytext);
+				break;
+			case NUMERO_REAL:
+				printf("%s - Numero real\n", yytext);
+				break;
+			case IDGRANDEDEMAIS:
+				printf("Erro: ID muito grande. Linha: %d\n", yylineno);
+				break;
+			case ID:
+				printf("%s - ID\n", yytext);
+				break;
+			case PONTO:
+				printf("%s - Ponto\n", yytext);
+				break;
+			case PONTOEVIRGULA:
+				printf("%s - Ponto e virgula\n", yytext);
+				break;
+			case ATRIBUICAO:
+				printf("%s - Atribuicao\n", yytext);
+				break;
+			case DOISPONTOS:
+				printf("%s - Dois pontos\n", yytext);
+				break;
+			case APARENTESES:
+				printf("%s - Parenteses aberto\n", yytext);
+				break;
+			case FPARENTESES:
+				printf("%s - Parenteses fechado\n", yytext);
+				break;
+			case VIRGULA:
+				printf("%s - Virgula\n", yytext);
+				break;
+			case MAIORIGUAL:
+				printf("%s - Maior igual\n", yytext);
+				break;
+			case MENORIGUAL:
+				printf("%s - Menor igual\n", yytext);
+				break;
+			case DIFERENTE:
+				printf("%s - Diferente\n", yytext);
+				break;
+			case MENOR:
+				printf("%s - Menor\n", yytext);
+				break;
+			case MAIOR:
+				printf("%s - Maior\n", yytext);
+				break;
+			case IGUAL:
+				printf("%s - Igual\n", yytext);
+				break;
+			case SOMA:
+				printf("%s - Soma\n", yytext);
+				break;
+			case SUBTRACAO:
+				printf("%s - Subtracao\n", yytext);
+				break;
+			case MULTIPLICACAO:
+				printf("%s - Multiplicacao\n", yytext);
+				break;
+			case DIVISAO:
+				printf("%s - Divisao\n", yytext);
+				break;
+			case NUMEROMALFORMADO:
+				printf("%s - Numero mal formado. Linha: %d\n", yytext, yylineno);
+				break;
+			case NUMEROMALFORMADO2:
+				printf("%s - Numero mal formado. Linha: %d\n", yytext, yylineno);
+				break;
+			case NUMEROMALFORMADO3:
+				printf("%s - Numero mal formado. Linha: %d\n", yytext, yylineno);
+				break;
+			case CARACTERINVALIDO:
+				printf("%s - Caracter invalido. Linha: %d\n", yytext, yylineno);
+				break;
+		}
+
+		ntoken = yylex();
+	}
+    return 0;
+}
+
+
+// Inicializa a hashTable alocando espaço
+int initTable() 
+{
+
+	hashTable = (hashmap*) malloc (HASHMAP_SIZE*sizeof(hashmap));
+	int i;
+	for(i = 0; i < HASHMAP_SIZE; i++)
+	{
+		hashTable[i].element = NULL;
+	}
+
+	FILE* fp;
+	char* word;
+	word = (char*) malloc (TOKEN_MAX_SIZE*sizeof(char));
+	// Abre o arquivo que contém as palavras reservadas
+	fp = fopen("palavras_reservadas.txt", "r");
+	// Verifica se conseguiu abrir
+	if(fp == NULL)
+	{
+		printf("ERROR: Não conseguiu ler o arquivo\n");
+		return 1;
+	}
+	
+	if(DEBUG) printf("Inicializando\n");
+
+	// Lê todas as palavras e guarda no hashmap
+	while(!feof(fp))
+	{
+		fgets(word, TOKEN_MAX_SIZE, fp);
+		
+		// É necessário apagar o \n que ele lê
+		// se não apagar, o código hash sai diferente
+		if(0 < strlen(word) && '\n' != word[0])
+        {
+            word[strlen(word) - 1] = '\0';
+        }
+		if(DEBUG) printf("Leu: %s\n", word);
+		if(DEBUG) printf("Que tem tamanho: %lu\n", strlen(word));
+		// Adiciona a palavra no hashmap e verifica se deu certo
+		if(addElement(word))
+		{
+			return 1;
+		}
+	}
+	
+	fclose(fp);
+	return 0;
+}
+
+// Passa a palavra pelo algoritmo para gerar o numero do hash dela
+// retorna esse numero
+int getHashCode(char* s)
+{
+	int hash = 0;
+	int i;
+	// Aplica o hash na palavra para obter o código dela
+	for (i = 0; i < strlen(s); i++)
+	    hash = (PRIME_NUMBER * hash + s[i]) % HASHMAP_SIZE;
+
+	if(DEBUG) printf("Hash Code de %s eh %d\n", s, hash);
+
+	return hash;
+}
+
+// Adiciona elemento na tabela hash
+// chama a função getHashCode que retorna o codigo hash de acordo
+// com a string passada. Verifica se aquela posição já foi ocupada.
+// Se não foi, adiciona na tabela
+int addElement(char* s)
+{
+	int hash;
+	hash = getHashCode(s);
+	if(hashTable[hash].element != NULL)
+	{
+		printf("ERROR: Colision. This hash already exists!\n");
+		return 1;
+	}
+
+	hashTable[hash].element = (char*) malloc (strlen(s)*sizeof(char));
+	strcpy(hashTable[hash].element, s);
+	if(DEBUG) printf("Elemento %s adicionado\n", hashTable[hash].element);
+
+	return 0;
+}
+
+// Busca o token que foi lido como ID no Flex
+// Se encontrar na tabela hash, ele imprime o token
+// Senão, ele diz que é um ID
+int searchElement(char* s)
+{
+	int hash = getHashCode(s);
+	if(hashTable[hash].element != NULL && !strcmp(hashTable[hash].element, s))
+	{
+		printf("%s - %s\n", s, hashTable[hash].element);
+		return PALAVRARESERVADA;
+	}
+	else
+	{
+		//printf("%s - ID\n", s);
+		return ID;
+	}
+}
